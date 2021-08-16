a11dcaaff4faa992e57d22fb2a16013305a8190c
CQun <1789570598@qq.com>
2021-08-16 17:30:11 +0800

    CQun first commit

---
+++ src/main/java/com/cqun/boot/BootHellowordApplication.java
@@ -0,0 +1,13 @@
+package com.cqun.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHellowordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootHellowordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cqun/boot/BootHellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqun.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootHellowordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
